c7f702d3187a1fe7c60c3fb0c4d6b6b37e9ab020
hashirsajid58200p <hs58200p@gmail.com>
2026-06-23 11:33:14 +0500

    feat: implement object types, type assertion, functions, interfaces, and classes

--- task02-typescript-tutorials/typescript-crash-course/src/index.ts
+++ task02-typescript-tutorials/typescript-crash-course/src/index.ts
@@ -1,37 +1,129 @@
 // Basic Types
 
 let id: number = 5;
-let company: string = "Traverly"
+let company: string = "Traverly";
 let isPublished: boolean = true;
-let x: any = "Hello"
+let x: any = "Hello";
 
-let ids: number [] = [1, 2, 3, 4, 5]
-let arr: any[] = [1, "Hello", true]
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, "Hello", true];
 
 //tuple
 
-let person: [number, string, boolean] = [1, "hello", true]
+let person: [number, string, boolean] = [1, "hello", true];
 
 //tuple array
 
-let employee: [number, string][] =[ [1, "Ali"], [2, "Ahmed"], [3, "Hasan"]]
-
+let employee: [number, string][] = [
+  [1, "Ali"],
+  [2, "Ahmed"],
+  [3, "Hasan"],
+];
 
 // unions
 
-let pid: string | number = "22"
+let pid: string | number = "22";
 
 //Enums
 
 enum Direction {
-    Up = 5,
-    Down,
-    Left,
-    Right
+  Up = 5,
+  Down,
+  Left,
+  Right,
 }
 
-console.log(Direction.Down)
+enum StringDirection {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
 
+console.log(Direction.Down);
+console.log(StringDirection.Left);
 
+//objects
+// const user: {
+//   id: number;
+//   name: string;
+// } = {
+//   id: 1,
+//   name: "Ali",
+// };
+
+// Better way to do this
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: "Ali",
+};
+
+// Type Assertion
+let cid: any = 1;
+
+// let customerId = <number>cid
+
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+// void
+
+function log(message: string | number): void {
+  console.log(message);
+}
+
+// interfaces
+
+interface UserInterface {
+  readonly id: number;
+  name: string;
+  age?: number;
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: "Ali",
+};
+
+//can't user interface with unions and parametives
+
+// interfaces with functions
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
+
+// classes
+
+class Person {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+  register() {
+    return `${this.name} is registered`;
+  }
+}
+
+const person1 = new Person(1, "Ali");
+// console.log(person1);
+console.log(person1.register());
+const person2 = new Person(2, "Mike");
+console.log(person2);
 // id = "5"
 // console.log(id)
